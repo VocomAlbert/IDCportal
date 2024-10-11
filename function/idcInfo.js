fc40fc842f20a04a4466cd675c581f5c4baffdbe
VocomAlbert <albert@vocom.com>
2024-10-11 10:18:54 +0800

    update

--- function/idcInfo.js
+++ function/idcInfo.js
@@ -4,6 +4,7 @@ const Mail = require('../idc_mail.js');
 const excel = require('exceljs');
 const wb = new excel.Workbook();
 const FS = require('fs');
+const PATH = require('path');
 require('dotenv').config({ path: PATH.resolve(__dirname, '../.env') });
 
 module.exports.getIdc_Info = async function (powerPrice_KW_Hour, powerPrice_KW_Month, price_AllIn, powerAvail_MW, rackAvail, region, country) {
