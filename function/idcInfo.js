6de8976d58c69e69499ffbef792c9a99afc58b19
VocomAlbert <albert@vocom.com>
2024-10-25 15:43:39 +0800

    hotfix

--- function/idcInfo.js
+++ function/idcInfo.js
@@ -5,7 +5,6 @@ const excel = require('exceljs');
 const wb = new excel.Workbook();
 const FS = require('fs');
 const PATH = require('path');
-
 require('dotenv').config({ path: PATH.resolve(__dirname, '../.env') });
 
 
